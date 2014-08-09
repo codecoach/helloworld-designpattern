44de30aff68704b2b44741a1c6611b361eaabe12
yanchangyou <yanchangyou@gmail.com>
2014-08-09 14:22:49 +0800

    移动项目

---
+++ src/main/java/org/code/helloworld/designpattern/App.java
@@ -0,0 +1,13 @@
+package org.code.helloworld.designpattern;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
