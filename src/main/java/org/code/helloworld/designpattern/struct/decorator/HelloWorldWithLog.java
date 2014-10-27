9057d5bb5c5a2fa122169af25866fda931ce91e4
yanchangyou <yanchangyou@gmail.com>
2014-10-27 16:47:29 +0800

    装饰模式 1， 去掉继承，使用组合

--- src/main/java/org/code/helloworld/designpattern/struct/decorator/HelloWorldWithLog.java
+++ src/main/java/org/code/helloworld/designpattern/struct/decorator/HelloWorldWithLog.java
@@ -6,11 +6,24 @@ package org.code.helloworld.designpattern.struct.decorator;
  * @author yanchangyou@gmail.com
  *
  */
-public class HelloWorldWithLog extends HelloWorld {
+public class HelloWorldWithLog {
 
+	private HelloWorld helloWorld;
+
+	public HelloWorldWithLog() {
+		helloWorld = new HelloWorld();
+	}
+
+	/**
+	 * 在说之前和之后附加行为：这和aop很类似
+	 */
 	public void speak() {
+
 		System.out.println("speak之前");
-		System.out.println(think());
+
+		helloWorld.speak();
+
 		System.out.println("speak之后");
+
 	}
 }
